81b25bf3777c756efdbc3a71de18a642f808d056
gamersubs <90562924+gamersubs@users.noreply.github.com>
2025-03-20 19:37:55 +0100

    removed unused code

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
   const dropdownToggle = document.getElementById('dropdownToggle');
   const dropdownMenu = document.getElementById('dropdownMenu');
-  const hamburger = document.getElementById('hamburger');
-  const menu = document.getElementById('menu');
 
   // Dropdown toggle
   dropdownToggle.addEventListener('click', function (e) {
@@ -10,17 +8,10 @@ document.addEventListener('DOMContentLoaded', function () {
     dropdownMenu.classList.toggle('show');
   });
 
-  // Hamburger toggle
-  hamburger.addEventListener('click', function () {
-    menu.classList.toggle('show');
-  });
-
   // Close dropdown when clicking outside
   window.addEventListener('click', function (e) {
-    if (!e.target.matches('#dropdownToggle')) {
-      if (dropdownMenu.classList.contains('show')) {
-        dropdownMenu.classList.remove('show');
-      }
+    if (!e.target.closest('#dropdownToggle')) {
+      dropdownMenu.classList.remove('show');
     }
   });
 });
